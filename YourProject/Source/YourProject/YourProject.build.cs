f43618b11bc5451c10cf4ff010c75cc81e85ee53
ArturNas <nasypovicartur1@gmail.com>
2024-10-13 22:20:10 +0200

    Update

---
+++ YourProject/Source/YourProject/YourProject.build.cs
@@ -0,0 +1,23 @@
+// YourProject/Source/YourProject/YourProject.Build.cs
+
+using UnrealBuildTool;
+
+public class YourProject : ModuleRules
+{
+    public YourProject(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        // Uncomment if using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
+}
